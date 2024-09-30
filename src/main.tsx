b4ff1b43865297c021aff59b6afd8863dab24b5d
Lucas Schuber <lucas.hammarstrand@hotmail.com>
2024-09-30 10:50:53 +0200

    added methods

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 // import './index.css'
 import "./assets/css/main.css"
+import "./assets/css/buttons.css"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+// import fotnawesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons'; 
+
 // imported images
 
 // imported css files
@@ -24,6 +28,7 @@ const Index = () => {
 
 
     useEffect(() => {
+      if (data.length === 0) {
         const fetchData = async () => {
           try {
             const response = await axios.get<{ data: DataArray[] }>('http://localhost:5000/api/alldata');
@@ -42,6 +47,7 @@ const Index = () => {
         };
       
         fetchData();
+      }
       }, []);
       
   
@@ -53,55 +59,89 @@ const Index = () => {
 
       const addRow = (project: DataArray) => {
         console.log('project', project);
-        setSelectedData((prevArray) => [...prevArray, project]);
+        const exists = selectedData.some(item => item.orderuuid === project.orderuuid);
+        if (exists) {
+          console.log("Project is already added")
+        } else {
+          setSelectedData((prevArray) => [...prevArray, project]);
+        }
       }
 
+      const deleteRow = (item: DataArray) => {
+        console.log('item', item);{
+          console.log("Project is already added")
+          setSelectedData((prevArray) => prevArray.filter(i => i.orderuuid !== item.orderuuid));
+        }
+      }
+      
+      const deleteAll = () => {
+          setSelectedData([]);
+      }
 
-      useEffect(() => {
-        console.log('selectedRow', selectedData);
-      }, [selectedData]);
 
 
 
   return (
     <div className='wrapper'>
+
     <div className='header-box'>
         <h6>POL - Proof Of Life</h6>
         <p>Your system to handle orders</p>
     </div>
-    <div className='table-wrapper'>
-    <table className='table ' >
-        <thead>
-          <tr>
-            <th>Order UUID</th>
-            <th>Portal UUID</th>
-            {/* <th>Paid</th> */}
-            <th>Inserted</th>
-            {/* <th>Updated</th> */}
-            <th>Originating</th>
-            {/* <th>Count</th> */}
-          </tr>
-        </thead>
-        <tbody>
-            {data.length > 0 ? (
-                data.map((item) => (
-                    <tr className='table-row' key={item.orderuuid} onClick={() => addRow(item)}>
-                        <td title={item.orderuuid}>{item.orderuuid.length > 15 ? item.orderuuid.substring(0, 15) + '...' : item.orderuuid}</td>
-                        <td> title={item.portaluuid}{item.portaluuid.length > 15 ? item.portaluuid.substring(0, 15) + '...' : item.portaluuid}</td>
-                        {/* <td>{item.paid}</td> */}
-                        <td>{new Date(item.inserted).toLocaleString().substring(0,10)}</td>
-                        {/* <td>{new Date(item.updated).toLocaleString().substring(0,10)}</td> */}
-                        <td>{getOriginatingPrefix(item.originating)}</td>
-                        {/* <td>{item.cnt !== null ? item.cnt : '-'}</td> */}
+    <div className='d-flex mt-5'>
+      <div className='table-wrapper'>
+        <table className='table ' >
+            <thead>
+              <tr>
+                <th>Order UUID</th>
+                <th>Portal UUID</th>
+                {/* <th>Paid</th> */}
+                <th>Inserted</th>
+                {/* <th>Updated</th> */}
+                <th>Originating</th>
+                {/* <th>Count</th> */}
+                <th>N.</th>
+                <th>P.R</th>
+                <th>Post</th>
+                <th>External</th>
+                <th>Cancel</th>
+                <th>Flag</th>
+              </tr>
+            </thead>
+            <tbody>
+                {data.length > 0 ? (
+                    data.map((item) => (
+                        <tr className='table-row' key={item.orderuuid} onClick={() => addRow(item)}>
+                            <td title={item.orderuuid}>{item.orderuuid.length > 15 ? item.orderuuid.substring(0, 15) + '...' : item.orderuuid}</td>
+                            <td title={item.portaluuid}>{item.portaluuid.length > 15 ? item.portaluuid.substring(0, 15) + '...' : item.portaluuid}</td>
+                            {/* <td>{item.paid}</td> */}
+                            <td>{new Date(item.inserted).toLocaleString().substring(0,10)}</td>
+                            {/* <td>{new Date(item.updated).toLocaleString().substring(0,10)}</td> */}
+                            <td>{getOriginatingPrefix(item.originating)}</td>
+                            {/* <td>{item.cnt !== null ? item.cnt : '-'}</td> */}
+                        </tr>
+                    ))
+                ) : (
+                    <tr>
+                        <td colSpan={7}>No data available</td>
                     </tr>
-                ))
-            ) : (
-                <tr>
-                    <td colSpan={7}>No data available</td>
-                </tr>
-            )}
-        </tbody>
-      </table>
+                )}
+            </tbody>
+          </table>
+        </div>
+
+        {selectedData.length > 0 && (
+        <div className='selected-data-box ml-5'>
+          <h6 className='mb-5' style={{ textDecoration: "underline" }}>Selected Data:</h6>
+            {selectedData && selectedData.map((item) => (
+                <div className='mb-4 d-flex' key={item.orderuuid}>
+                  <FontAwesomeIcon icon={faTimes} title="Remove Project" className='delete-selecteddata-button' onClick={() => deleteRow(item)} />
+                  <h6 className='mx-3'>{item.orderuuid}</h6>
+                </div>
+              ))}
+            <h6 className='delete-all-button' title='Remove All Projects' onClick={() => deleteAll()}>Remove All</h6>  
+        </div>
+        )}
       </div>
     </div>
   );
